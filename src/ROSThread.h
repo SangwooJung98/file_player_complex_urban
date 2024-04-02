c2dba475d9163d5fab00827d0335031b3ee09678
SangwooJung98 <dan0130@snu.ac.kr>
2024-04-02 16:19:33 +0900

    opencv4 compatible

--- src/ROSThread.h
+++ src/ROSThread.h
@@ -17,8 +17,10 @@
 #include <algorithm>
 #include <ros/ros.h>
 #include <ros/time.h>
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+// #include <opencv/cv.h>
+// #include <opencv/highgui.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <image_transport/image_transport.h>
